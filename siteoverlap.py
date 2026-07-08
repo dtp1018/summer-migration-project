fa9c49c7dff8f933df33de01e87d1faa0866051a
dtp1018 <dtp1018@usnh.edu>
2026-07-08 17:31:05 -0400

    Created a data set that excludes all sites that are not repeated - went from 42700 rows to 25700

--- siteoverlap.py
+++ siteoverlap.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from cleaningdata1 import excluded_dataset, new_dataset
+from cleaningdata1 import new_dataset
 
 site_counts = (new_dataset.groupby(["SCIENTIFIC_NAME", "STATION"]).size().reset_index(name='count').sort_values(by=["SCIENTIFIC_NAME", "STATION"], ascending=[True, False]))
 #site_counts = new_dataset["STATION"].value_counts()
@@ -10,7 +10,7 @@ site_counts.to_csv('sitecounts')
 total_species = 8
 overlapping_sites_count = (new_dataset.groupby('STATION').filter(lambda x: x['SCIENTIFIC_NAME'].nunique() == total_species))
 overlapping_sites_list = overlapping_sites_count['STATION'].unique().tolist()
-#print(overlapping_sites_list)
+print(overlapping_sites_list)
 #print(overlapping_sites_count)
 #overlapping_sites_list.to_csv('overlappingsiteslist')
 overlapping_sites_count.to_csv('overlappingsitesmoredetail')
