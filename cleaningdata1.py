cdfd4302ccc407ecc026b248a3fbd9807a6dad42
dtp1018 <dtp1018@usnh.edu>
2026-07-08 09:38:32 -0400

    Excluded some very high data points to see what would happen with the scatterplot, attempting to add trend lines

--- cleaningdata1.py
+++ cleaningdata1.py
@@ -6,3 +6,15 @@ min_value = new_dataset['EXPCATCHNUM'].min()
 max_value = new_dataset['EXPCATCHNUM'].max()
 max_location = new_dataset['EXPCATCHNUM'].idxmax()
 print(min_value, max_value, max_location)
+
+excluded_dataset = new_dataset[new_dataset['EXPCATCHNUM'] != 168036.0]
+excluded_dataset = excluded_dataset[excluded_dataset['EXPCATCHNUM'] != 153128.0]
+excluded_dataset = excluded_dataset[excluded_dataset['EXPCATCHNUM'] != 99693.0]
+excluded_dataset = excluded_dataset[excluded_dataset['EXPCATCHNUM'] != 77473.0]
+excluded_dataset = excluded_dataset[excluded_dataset['EXPCATCHNUM'] != 64968.0]
+excluded_dataset.to_csv('outlier_sampleset.csv')
+
+excluded_min_value = excluded_dataset['EXPCATCHNUM'].min()
+excluded_max_value = excluded_dataset['EXPCATCHNUM'].max()
+excluded_max_location = excluded_dataset['EXPCATCHNUM'].idxmax()
+print(excluded_min_value, excluded_max_value, excluded_max_location)
--- scatterplottest.py
+++ scatterplottest.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt
 from uploadingcsvfile import new_dataset
+from cleaningdata1 import excluded_dataset
+import pandas as pd
 
-new_dataset.plot(kind = 'scatter', x = 'CRUISE6', y = 'EXPCATCHNUM', c = 'SCIENTIFIC_NAME')
-plt.savefig("ScatterplotFirstAttempt")
+excluded_dataset.plot(kind = 'scatter', x = 'CRUISE6', y = 'EXPCATCHNUM', c = 'SCIENTIFIC_NAME')
+plt.savefig("ScatterplotExcludedAttempt")
+#plt.show()
+
+#minidata = pd.DataFrame(excluded_dataset)
+#for name, group in minidata.groupby("SCIENTIFIC_NAME"):
+#    ax.plot(group["CRUISE6"], group["EXPCATCHNUM"], label=name)
+
 plt.show()
