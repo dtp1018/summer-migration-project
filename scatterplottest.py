ddf07f17db2eb33fd2d423214dbf6c163ffa6c5e
dtp1018 <dtp1018@usnh.edu>
2026-07-09 09:20:11 -0400

    trying out ways to get trend lines

--- scatterplottest.py
+++ scatterplottest.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 from uploadingcsvfile import new_dataset
-from cleaningdata1 import excluded_dataset
+from cleaningdata2 import new_excluded_dataset
 import pandas as pd
+import numpy as np
 
-excluded_dataset.plot(kind = 'scatter', x = 'CRUISE6', y = 'EXPCATCHNUM', c = 'SCIENTIFIC_NAME')
-plt.savefig("ScatterplotExcludedAttempt")
+ax = new_excluded_dataset.plot(kind = 'scatter', x = 'CRUISE6', y = 'EXPCATCHNUM', c = 'SCIENTIFIC_NAME')
+slope, intercept = np.polyfit(new_excluded_dataset['CRUISE6'], new_excluded_dataset['EXPCATCHNUM'], 1)
+trend_y = slope * new_excluded_dataset['CRUISE6'] + intercept
+ax.plot(new_excluded_dataset['CRUISE6'], trend_y, color='red', linestyle='--', label='Trend Line')
+
+plt.savefig("ScatterplotNewExcludedAttempt")
 #plt.show()
 
 #minidata = pd.DataFrame(excluded_dataset)
